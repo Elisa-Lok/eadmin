676e3421a31ba71cf85d0bf332ed388b7e63b0a1
Elisa <elisalok413@gmail.com>
2024-01-21 16:34:21 +1300

    feat: update edit form

---
+++ src/Data/Data.js
@@ -0,0 +1,116 @@
+// Sidebar imports
+import {
+  UilEstate,
+  UilClipboardAlt,
+  UilUsersAlt,
+  UilPackage,
+  UilChart,
+} from "@iconscout/react-unicons";
+
+// Analytics Cards imports
+import { UilUsdSquare, UilMoneyWithdrawal } from "@iconscout/react-unicons";
+//import { keyboard } from "@testing-library/user-event/dist/keyboard";
+
+// Recent Card Imports
+import img1 from "../imgs/img1.png";
+import img2 from "../imgs/img2.png";
+import img3 from "../imgs/img3.png";
+
+// Sidebar Data
+export const SidebarData = [
+  {
+    icon: UilEstate,
+    heading: "Dashboard",
+  },
+  {
+    icon: UilClipboardAlt,
+    heading: "Orders",
+  },
+  {
+    icon: UilUsersAlt,
+    heading: "Customers",
+  },
+  {
+    icon: UilPackage,
+    heading: 'Products'
+  },
+  {
+    icon: UilChart,
+    heading: 'Analytics'
+  },
+];
+
+// Analytics Cards Data
+export const cardsData = [
+  {
+    title: "Sales",
+    color: {
+      backGround: "linear-gradient(180deg, #194561 0%, #1484c3 100%)",
+      boxShadow: "0px 10px 20px 0px #c0c6f5",
+    },
+    barValue: 70,
+    value: "32,850",
+    png: UilUsdSquare,
+    series: [
+      {
+        name: "Sales",
+        data: [31, 40, 28, 51, 42, 109, 90],
+      },
+    ],
+  },
+  {
+    title: "Revenue",
+    color: {
+      backGround: "linear-gradient(180deg, #BB67FF 0%, #FC929C 100%)",
+      boxShadow: "0px 10px 20px 0px #FDC0C7",
+    },
+    barValue: 80,
+    value: "19,320",
+    png: UilMoneyWithdrawal,
+    series: [
+      {
+        name: "Revenue",
+        data: [10, 100, 50, 70, 80, 30, 40],
+      },
+    ],
+  },
+  {
+    title: "Expenses",
+    color: {
+      backGround:
+        "linear-gradient(rgb(348, 112, 54) -146.42%, rgb(243 182 132) -46.42%)",
+      boxShadow: "0px 10px 20px 0px #F9D59B",
+    },
+    barValue: 60,
+    value: "4,270",
+    png: UilClipboardAlt,
+    series: [
+      {
+        name: "Expenses",
+        data: [10, 25, 15, 30, 12, 15, 20],
+      },
+    ],
+  },
+];
+
+// Recent Update Card Data
+export const UpdatesData = [
+  {
+    img: img1,
+    name: "Andrew Thomas",
+    noti: "has ordered Apple smart watch 2500mh battery.",
+    time: "18 seconds ago",
+  },
+  {
+    img: img2,
+    name: "James Bond",
+    noti: "has received Samsung gadget for charging battery.",
+    time: "25 minutes ago",
+  },
+  {
+    img: img3,
+    name: "Iron Man",
+    noti: "has ordered Apple smart watch, samsung Gear 2500mh battery.",
+    time: "3 hours ago",
+  },
+];
